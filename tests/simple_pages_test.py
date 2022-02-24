86564faced28a82f551ad9611cdd19bf4e30ee3e
tomcat02 <hjp4@hjp.edu>
2022-02-24 11:16:19 -0500

    first set v1

---
+++ tests/simple_pages_test.py
@@ -0,0 +1,19 @@
+"""This test the homepage"""
+
+def test_request_index(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Index Page" in response.data
+
+def test_request_about(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"About Page" in response.data
+
+def test_request_page1(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"APage 1" in response.data
